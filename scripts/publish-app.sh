bfcbdb9b937c849363b8fd55e74dea5e74d01514
Vinny Martins <martins.vds@outlook.com>
2022-06-17 12:17:27 -0600

    add publish-app script

--- scripts/publish-app.sh
+++ scripts/publish-app.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+set -ex
+
 dotnet publish ./app/KnowledgeMining.UI/KnowledgeMining.UI.csproj -c Release -o ./dist/linux-x64/ -r linux-x64
 
 pushd ./dist/linux-x64
 
-zip -r app.linux-x64.zip ./
+zip -r -q -9 ../app.linux-x64.zip *
 
 popd
