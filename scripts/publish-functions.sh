490379d0e643abdfd38d554a2a3a059e11c523e8
Vinny Martins <martins.vds@outlook.com>
2022-06-20 16:32:01 -0600

    rename scripts

---
+++ scripts/publish-functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+dotnet publish ./app/KnowledgeMining.Functions.Skills/KnowledgeMining.Functions.Skills.csproj -c Release -o ./dist/skills/linux-x64/ -r linux-x64 -f 'net6.0' --self-contained
+
+pushd ./dist/skills/linux-x64
+
+zip -r -q -9 ../../skills.linux-x64.zip *
+
+popd
