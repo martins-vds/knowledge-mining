1ab3c5a29d0d75125dbdd665b78330e06b1df5d3
Vinny Martins <martins.vds@outlook.com>
2022-07-06 13:01:57 -0600

    add sleep

--- scripts/upload-thesaurus.sh
+++ scripts/upload-thesaurus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## USAGE  ./upload-thesaurus.sh <RESOURCE GROUP> <STORAGE ACCOUNT NAME> <SYNONMS CONTAINER> <THESAURUS FILE PATH>
-set -e
+set -ex
 
 function trim(){
     local string=$(echo $1 | tr -d '[[='"'"'=]]')
@@ -16,6 +16,8 @@ thesaurus_file_path=$(trim "$4")
 ip_address=$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')
 
 az storage account network-rule add -g $rg_name --account-name $storage_account_name --ip-address $ip_address
+
+sleep 5
 
 az storage blob upload --account-name $storage_account_name -f $thesaurus_file_path -c $synonyms_container -n thesaurus.json --overwrite
 
